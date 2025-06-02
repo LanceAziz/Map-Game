b7b786346b0c8d5bf8dba15a4f83fae0e84eb223
lance aziz <lanceaziz2002@gmail.com>
2025-06-03 02:07:45 +0300

    svgs

--- src/app/assets/svgs.js
+++ src/app/assets/svgs.js
@@ -1,9 +1,91 @@
 import medal1 from "../assets/ranks/medal1.svg"
 import medal2 from "../assets/ranks/medal2.svg"
 import medal3 from "../assets/ranks/medal3.svg"
+import outline from "../assets/map/ouline.svg"
+import asia1 from "../assets/map/asia1.svg"
+import asia2 from "../assets/map/asia2.svg"
+import asia3 from "../assets/map/asia3.svg"
+import asia4 from "../assets/map/asia4.svg"
+import asia5 from "../assets/map/asia5.svg"
+import asia6 from "../assets/map/asia6.svg"
+import asia7 from "../assets/map/asia7.svg"
+import asia8 from "../assets/map/asia8.svg"
+import asia9 from "../assets/map/asia9.svg"
+import asia10 from "../assets/map/asia10.svg"
+import asia11 from "../assets/map/asia11.svg"
+import africa1 from "../assets/map/africa1.svg"
+import africa2 from "../assets/map/africa2.svg"
+import africa3 from "../assets/map/africa3.svg"
+import africa4 from "../assets/map/africa4.svg"
+import africa5 from "../assets/map/africa5.svg"
+import africa6 from "../assets/map/africa6.svg"
+import australia1 from "../assets/map/australia1.svg"
+import australia2 from "../assets/map/australia2.svg"
+import australia3 from "../assets/map/australia3.svg"
+import europe1 from "../assets/map/europe1.svg"
+import europe2 from "../assets/map/europe2.svg"
+import europe3 from "../assets/map/europe3.svg"
+import europe4 from "../assets/map/europe4.svg"
+import europe5 from "../assets/map/europe5.svg"
+import europe6 from "../assets/map/europe6.svg"
+import europe7 from "../assets/map/europe7.svg"
+import northAmerica1 from "../assets/map/northAmerica1.svg"
+import northAmerica2 from "../assets/map/northAmerica2.svg"
+import northAmerica3 from "../assets/map/northAmerica3.svg"
+import northAmerica4 from "../assets/map/northAmerica4.svg"
+import northAmerica5 from "../assets/map/northAmerica5.svg"
+import northAmerica6 from "../assets/map/northAmerica6.svg"
+import northAmerica7 from "../assets/map/northAmerica7.svg"
+import northAmerica8 from "../assets/map/northAmerica8.svg"
+import northAmerica9 from "../assets/map/northAmerica9.svg"
+import southAmerica1 from "../assets/map/southAmerica1.svg"
+import southAmerica2 from "../assets/map/southAmerica2.svg"
+import southAmerica3 from "../assets/map/southAmerica3.svg"
+import southAmerica4 from "../assets/map/southAmerica4.svg"
 
 export const Svgs = {
     medal1,
     medal2,
-    medal3
+    medal3,
+    outline,
+    asia1,
+    asia2,
+    asia3,
+    asia4,
+    asia5,
+    asia6,
+    asia7,
+    asia8,
+    asia9,
+    asia10,
+    asia11,
+    africa1,
+    africa2,
+    africa3,
+    africa4,
+    africa5,
+    africa6,
+    australia1,
+    australia2,
+    australia3,
+    europe1,
+    europe2,
+    europe3,
+    europe4,
+    europe5,
+    europe6,
+    europe7,
+    northAmerica1,
+    northAmerica2,
+    northAmerica3,
+    northAmerica4,
+    northAmerica5,
+    northAmerica6,
+    northAmerica7,
+    northAmerica8,
+    northAmerica9,
+    southAmerica1,
+    southAmerica2,
+    southAmerica3,
+    southAmerica4,
 }
