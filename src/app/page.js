767d6e85b332e38260d0e9ee199873cd71dbb427
lance aziz <lanceaziz2002@gmail.com>
2025-06-03 02:17:21 +0300

    map outline in place

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,21 @@
 import Card from "./components/card";
 import { teams } from "./data/data";
+import { Svgs } from "./assets/svgs";
 
 export default function Home() {
 
   return (
-    <>
+    <div className="d-flex">
       <div className="col-lg-4 col-md-6 p-1">
         {teams.map((team) => {
           return <Card key={team.id} team={team} />
         })}
       </div>
-    </>
+      <div className="col-lg-8 col-md-6 d-flex justify-content-center align-items-center">
+        <div className="w-75">
+          <Svgs.outline />
+        </div>
+      </div>
+    </div>
   );
 }
