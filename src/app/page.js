3ef2347a13c047fd4c72bc80c3d4c9c52f24a383
lance aziz <lanceaziz2002@gmail.com>
2025-06-03 08:54:03 +0300

    contry placing

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Card from "./components/card";
 import { teams } from "./data/data";
 import { Svgs } from "./assets/svgs";
+import styles from "./page.module.css"
 
 export default function Home() {
 
@@ -12,8 +13,13 @@ export default function Home() {
         })}
       </div>
       <div className="col-lg-8 col-md-6 d-flex justify-content-center align-items-center">
-        <div className="w-75">
-          <Svgs.outline />
+        <div className="position-absolute w-50">
+          <div className={styles.outline}>
+            <Svgs.outline />
+          </div>
+          <div className={styles.country}>
+            <Svgs.africa1 />
+          </div>
         </div>
       </div>
     </div>
